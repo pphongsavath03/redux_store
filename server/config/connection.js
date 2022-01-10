1e22e27348bdec29143e66765add46f31d05bd78
pphong <pphongsavath03@gmail.com>
2022-01-10 14:22:54 -0500

    connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
-  useNewUrlParser: true,
+  useNewUrlParser: false,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
